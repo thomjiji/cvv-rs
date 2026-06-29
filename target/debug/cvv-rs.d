c43623a6d7d5093fdc2b5a8ca30f47cd64255270
thomjiji <hxy449999@gmail.com>
2026-06-29 21:32:47 +0800

    chore: remove target/ from tracking

--- target/debug/cvv-rs.d
+++
@@ -1,1 +0,0 @@
-C:\Users\thom\git\cvv\cvv-rs\target\debug\cvv-rs.exe: C:\Users\thom\git\cvv\cvv-rs\src\main.rs
--- target/debug/deps/cvv_rs.d
+++
@@ -1,5 +0,0 @@
-C:\Users\thom\git\cvv\cvv-rs\target\debug\deps\cvv_rs.d: src\main.rs
-
-C:\Users\thom\git\cvv\cvv-rs\target\debug\deps\cvv_rs.exe: src\main.rs
-
-src\main.rs:
